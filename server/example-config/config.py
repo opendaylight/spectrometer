6bf77f5ef0e6453fc10250d15fd50ee3c962ce0c
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-05-07 00:13:26 -0400

    Add logging support
    Use the Python logging module to log to console and file as appropriate
    when run in production mode. Also convert all print statements over to
    logger.
    
    Change-Id: I0e0f76687a48b2f3679e92e4a52ca776ae376850
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- server/example-config/config.py
+++ server/example-config/config.py
@@ -4,6 +4,7 @@ MONGO_HOST = '127.0.0.1'
 MONGO_PORT = 27017
 DEBUG = True
 
+LOG_DIR = '/var/log/spectrometer'
 GERRIT_URL = 'https://git.opendaylight.org/gerrit/'
 MIRROR_DIR = 'git/'
 MIRROR_INTERVAL = 300  # Interval to mirror repos in seconds
